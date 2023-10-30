2f2b273116449fa72ed25b9f91468ca1cc577836
ZIUGOD <caique.dubstep@gmail.com>
2023-10-30 01:22:52 -0300

    att

--- src/pages/hub/index.tsx
+++ src/pages/hub/index.tsx
@@ -1,8 +1,10 @@
 // Importações das bibliotecas necessárias
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TouchableOpacity, View, VirtualizedList, Text } from "react-native";
+import Slider from "@react-native-community/slider";
 import api from "../../services/api";
 import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Define o tipo de objeto para representar as Coins
 type CoinType = {
@@ -164,48 +166,75 @@ function renderItem({ item }: { item: CoinType }) {
 // Componente principal que busca e exibe a lista de Coins
 export default function Hub() {
     const [Coins, setCoins] = useState<CoinType[]>([]);
+    const [coinCount, setCoinCount] = useState<number>(10); // Defina o tipo como número
 
-    // Função para buscar dados da API quando o componente é montado
     const getData = async () => {
         try {
             const response = await api.get("/v1/cryptocurrency/listings/latest");
             const { data }: { data: CoinType[] } = response.data;
 
-            setCoins(data);
-        } catch (error) {
-            setCoins([]); // Limpa a lista em caso de erro
-        }
-    };
+            // Limitar o número de moedas exibidas com base no valor de coinCount
+            const limitedData = data.slice(0, coinCount);
 
-    // Chamada para buscar dados quando o componente é montado
+            setCoins(limitedData);
+
+            // Armazene os dados localmente usando AsyncStorage
+            await AsyncStorage.setItem("coinsData", JSON.stringify(limitedData));
+        } catch (error) {
+            // Em caso de erro, tente recuperar os dados armazenados localmente
+            const storedData = await AsyncStorage.getItem("coinsData");
+            if (storedData) {
+                setCoins(JSON.parse(storedData));
+            } else {
+                setCoins([]); // Caso não haja dados locais, limpe a lista
+            }
+        }
+    }
+
     useEffect(() => {
         getData();
-    }, []);
+    }, [coinCount]); // Atualize os dados quando coinCount muda
 
     return (
-        <VirtualizedList
-            data={Coins}
-            keyExtractor={(item) => String(item.id)}
-            renderItem={renderItem}
-            getItemCount={() => Coins.length}
-            getItem={(data, index) => data[index]}
-            ItemSeparatorComponent={() => (
-                <View
-                    style={{
-                        height: 0,
-                        backgroundColor: "#ddd",
-                        marginVertical: 10,
-                        alignSelf: "center",
-                        width: "10%",
-                    }}
-                />
-            )}
-            refreshing={false}
-            onRefresh={() => getData()} // Função para recarregar os dados ao puxar para baixo
-            style={{
-                paddingHorizontal: 20,
-                paddingTop: 10,
-            }}
-        />
+        <View>
+            {/* Controle deslizante para selecionar a quantidade de moedas */}
+            <Slider
+                style={{ width: 300 }}
+                minimumValue={10}
+                maximumValue={100}
+                step={1}
+                value={coinCount}
+                onValueChange={(value) => setCoinCount(value)}
+                thumbTintColor="#FF5733" // Cor do botão deslizante
+                minimumTrackTintColor="#FF5733" // Cor da trilha à esquerda
+                maximumTrackTintColor="#C0C0C0" // Cor da trilha à direita
+            />
+
+            <Text>Mostrar {coinCount} moedas</Text>
+            <VirtualizedList
+                data={Coins}
+                keyExtractor={(item) => String(item.id)}
+                renderItem={renderItem}
+                getItemCount={() => Coins.length}
+                getItem={(data, index) => data[index]}
+                ItemSeparatorComponent={() => (
+                    <View
+                        style={{
+                            height: 0,
+                            backgroundColor: "#ddd",
+                            marginVertical: 10,
+                            alignSelf: "center",
+                            width: "10%",
+                        }}
+                    />
+                )}
+                refreshing={false}
+                onRefresh={() => getData()} // Função para recarregar os dados ao puxar para baixo
+                style={{
+                    paddingHorizontal: 20,
+                    paddingTop: 10,
+                }}
+            />
+        </View>
     );
 }
